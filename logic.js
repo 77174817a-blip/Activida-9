f836e6b4f95f3dd9434dc7775601b0d39d1099d7
Andres Lima <77174817a@mail.com>
2026-01-21 21:04:17 -0400

    Rompo tests intencionalmente

--- logic.js
+++ logic.js
@@ -1,8 +1,6 @@
-// logic.js
 function determinarEstado(nota) {
   if (nota < 0 || nota > 100) return "ERROR: Nota inválida";
   if (typeof nota !== 'number') return "ERROR: Debe ser un número";
   if (nota >= 70) return "REPROBADO";  // ❌ cambio intencional
-  return "APROBADO";                   // ❌ cambio intencional
+  return "APROBADO";
 }
-
